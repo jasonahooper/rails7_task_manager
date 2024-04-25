aa5c604ce71927c4f7484b27966623bb2f5ebb08
Jason Hooper <jasonahooper@gmail.com>
2024-04-25 16:40:10 +0100

    end of chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,30 @@ Rails.application.routes.draw do
   match 'hello', to: "main#hello", via: :get
 
   get 'main/index'
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/edit'
+
+  resources :categories do
+    member do
+      get :delete
+    end
+  end
+
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'tasks/index'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  # get 'tasks/show'
+  # get 'tasks/delete'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
+  # get 'categories/delete'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
