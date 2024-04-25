d1349a8a4d2290812c6a1fa65250f5d741f2d9ea
Jason Hooper <jasonahooper@gmail.com>
2024-04-25 12:00:02 +0100

    end of chapter 2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,16 @@
 class MainController < ApplicationController
   def index
+    # render 'index'
+  end
+
+  def about
+    @created_by = "Jason"
+    @id = params['id']
+    @page = params[:page]
+    # render 'about'
+  end
+
+  def hello
+    redirect_to action: 'index'
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   def index
+    @page = params[:page].to_i
+    @tasks = ['Fold laundry', 'Sweep porch', 'Wash dishes', 'Mow lawn']
   end
 
   def new
